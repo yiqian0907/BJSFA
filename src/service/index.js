5aa49d928e80b83f9f1d30f745f367189fca4abf
aicoder <malun666@126.com>
2019-05-03 15:46:34 +0800

    fixed the browserlistrc bug & add hotcss to lib

--- src/service/index.js
+++ src/service/index.js
@@ -16,27 +16,27 @@ axios.interceptors.request.use(
 );
 export default {
   login(data) {
-    return axios.post("http://143.167.209.27:45560/login", data);
+    return axios.post("http://localhost:45560/login", data);
   },
   getUserData() {
-    return axios.get("http://143.167.209.27:45560/api/user");
+    return axios.get("http://localhost:45560/api/user");
   },
   getNotices() {
-    return axios.get("http://143.167.209.27:45560/api/notices");
+    return axios.get("http://localhost:45560/api/notices");
   },
   getStoresInfo() {
-    return axios.get("http://143.167.209.27:45560/api/stores");
+    return axios.get("http://localhost:45560/api/stores");
   },
   getStorePerformance(id) {
-    return axios.get(`http://143.167.209.27:45560/api/performance/${id}`);
+    return axios.get(`http://localhost:45560/api/performance/${id}`);
   },
   getProductListsInfo() {
-    return axios.get("http://143.167.209.27:45560/api/products");
+    return axios.get("http://localhost:45560/api/products");
   },
   getWarehouse() {
-    return axios.get("http://143.167.209.27:45560/api/warehouse");
+    return axios.get("http://localhost:45560/api/warehouse");
   },
   submitOrder(data) {
-    return axios.post("http://143.167.209.27:45560/api/orders", data);
+    return axios.post("http://localhost:45560/api/orders", data);
   }
 };
